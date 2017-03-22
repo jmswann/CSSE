a5a77b67dba3ce58bda6cc52694b80836dac9bf9
Jeromy Swann <JMS0085@auburn.edu>
2017-03-22 16:51:21 -0500

    Green Light |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -1,3 +1,5 @@
+import math
+
 def dispatch(values=None):
 
     #Validate parm
@@ -11,6 +13,29 @@ def dispatch(values=None):
 
     #Perform designated function
     if(values['op'] == 'adjust'):
+        observation = values['observation']
+        height = 0
+        temperature = 72
+        pressure = 1010
+        horizon = 'natural'
+        x = int(observation.split('d')[0])
+        print x
+        y = float(observation.split('d')[1])
+        print y
+        observedAngle = x + (y / 60.0)
+        observedAngleInRadians = observedAngle * math.pi / 180.0
+        dip = (-0.97 * math.sqrt(height)) / 60
+        refraction = (-0.00452 * pressure) / (273 + convertFToC(temperature)) / math.tan(observedAngleInRadians)
+        print (-0.00452 * pressure)
+        print (273 + convertFToC(temperature))
+        print math.tan(observedAngleInRadians)
+        print refraction
+        adjustedAltitude = observedAngle + dip + refraction
+        print adjustedAltitude
+        adjustedAltitudeX = int(adjustedAltitude)
+        adjustedAltitudeY = round((adjustedAltitude - adjustedAltitudeX) * 60.0, 1)
+        adjustedAltitudeString = str(adjustedAltitudeX) + 'd' + str(adjustedAltitudeY)
+        values['altitude'] = adjustedAltitudeString
         return values    #<-------------- replace this with your implementation
     elif(values['op'] == 'predict'):
         return values    #This calculation is stubbed out
@@ -21,3 +46,6 @@ def dispatch(values=None):
     else:
         values['error'] = 'op is not a legal operation'
         return values
+
+def convertFToC(temperatureInF):
+    return (temperatureInF - 32) * 5.0 / 9.0
