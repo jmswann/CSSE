2c285c38e6fb7053ba0c9cdcf72268116ceb7d7a
Jeromy Swann <JMS0085@auburn.edu>
2017-04-07 23:35:20 -0500

    Refactor |

--- softwareprocess/dispatch.py
+++ softwareprocess/dispatch.py
@@ -16,80 +16,7 @@ def dispatch(values=None):
 
     #Perform designated function
     if(values['op'] == 'adjust'):
-        if 'altitude' in values:
-            values['error'] = 'altitude already in dictionary'
-            return values
-        if (not('observation' in values)):
-            values['error'] = 'no observation is specified'
-            return values
-        observation = values['observation']
-        if 'd' not in observation:
-            values['error'] = 'bad observation'
-            return values
-
-        #default values
-        height = 0
-        temperature = 72
-        pressure = 1010
-        horizon = 'natural'
-
-        if ('height' in values):
-            try:
-                height = float(values['height'])
-            except ValueError:
-                values['error'] = 'non-numeric height'
-                return values
-        if ('temperature' in values):
-            try:
-                temperature = int(values['temperature'])
-            except ValueError:
-                values['error'] = 'non-integer temperature'
-                return values
-        if ('pressure' in values):
-            try:
-                pressure = int(values['pressure'])
-            except ValueError:
-                values['error'] = 'non-integer pressure'
-                return values
-        if ('horizon' in values):
-            horizon = values['horizon'].lower()
-            if horizon != 'natural' and horizon != 'artificial':
-                values['error'] = 'invalid horizon type'
-                return values
-
-        if height < 0:
-            values['error'] = 'height out of bounds'
-            return values
-        if temperature < -20 or temperature > 120:
-            values['error'] = 'temperature out of bounds'
-            return values
-        if pressure < 100 or pressure > 1100:
-            values['error'] = 'pressure out of bounds'
-            return values
-
-        x = 0
-        y = 0.0
-        try:
-            x = int(observation.split('d')[0])
-            y = float(observation.split('d')[1])
-        except ValueError:
-            values['error'] = 'bad observation'
-            return values
-        if x < 0 or x >= 90 or y < 0 or y >= 60.0:
-            values['error'] = 'observation angle out of bounds'
-            return values
-
-        observedAngle = x + (y / 60.0)
-        observedAngleInRadians = observedAngle * math.pi / 180.0
-        dip = 0
-        if (horizon == 'natural'):
-            dip = (-0.97 * math.sqrt(height)) / 60
-        refraction = (-0.00452 * pressure) / (273 + convertFToC(temperature)) / math.tan(observedAngleInRadians)
-        adjustedAltitude = observedAngle + dip + refraction
-        adjustedAltitudeX = int(adjustedAltitude)
-        adjustedAltitudeY = round((adjustedAltitude - adjustedAltitudeX) * 60.0, 1)
-        adjustedAltitudeString = str(adjustedAltitudeX) + 'd' + str(adjustedAltitudeY)
-        values['altitude'] = adjustedAltitudeString
+        adjust(values)
         return values
     elif(values['op'] == 'predict'):
         if not('body' in values):
@@ -194,3 +121,79 @@ def dispatch(values=None):
 
 def convertFToC(temperatureInF):
     return (temperatureInF - 32) * 5.0 / 9.0
+
+def adjust(values):
+    if 'altitude' in values:
+        values['error'] = 'altitude already in dictionary'
+        return values
+    if (not('observation' in values)):
+        values['error'] = 'no observation is specified'
+        return values
+    observation = values['observation']
+    if 'd' not in observation:
+        values['error'] = 'bad observation'
+        return values
+
+    #default values
+    height = 0
+    temperature = 72
+    pressure = 1010
+    horizon = 'natural'
+
+    if ('height' in values):
+        try:
+            height = float(values['height'])
+        except ValueError:
+            values['error'] = 'non-numeric height'
+            return values
+    if ('temperature' in values):
+        try:
+            temperature = int(values['temperature'])
+        except ValueError:
+            values['error'] = 'non-integer temperature'
+            return values
+    if ('pressure' in values):
+        try:
+            pressure = int(values['pressure'])
+        except ValueError:
+            values['error'] = 'non-integer pressure'
+            return values
+    if ('horizon' in values):
+        horizon = values['horizon'].lower()
+        if horizon != 'natural' and horizon != 'artificial':
+            values['error'] = 'invalid horizon type'
+            return values
+
+    if height < 0:
+        values['error'] = 'height out of bounds'
+        return values
+    if temperature < -20 or temperature > 120:
+        values['error'] = 'temperature out of bounds'
+        return values
+    if pressure < 100 or pressure > 1100:
+        values['error'] = 'pressure out of bounds'
+        return values
+
+    x = 0
+    y = 0.0
+    try:
+        x = int(observation.split('d')[0])
+        y = float(observation.split('d')[1])
+    except ValueError:
+        values['error'] = 'bad observation'
+        return values
+    if x < 0 or x >= 90 or y < 0 or y >= 60.0:
+        values['error'] = 'observation angle out of bounds'
+        return values
+
+    observedAngle = x + (y / 60.0)
+    observedAngleInRadians = observedAngle * math.pi / 180.0
+    dip = 0
+    if (horizon == 'natural'):
+        dip = (-0.97 * math.sqrt(height)) / 60
+    refraction = (-0.00452 * pressure) / (273 + convertFToC(temperature)) / math.tan(observedAngleInRadians)
+    adjustedAltitude = observedAngle + dip + refraction
+    adjustedAltitudeX = int(adjustedAltitude)
+    adjustedAltitudeY = round((adjustedAltitude - adjustedAltitudeX) * 60.0, 1)
+    adjustedAltitudeString = str(adjustedAltitudeX) + 'd' + str(adjustedAltitudeY)
+    values['altitude'] = adjustedAltitudeString
