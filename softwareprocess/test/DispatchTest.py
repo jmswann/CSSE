dab1c0812746b52617e50c15c94813e944e93aa9
Jeromy Swann <JMS0085@auburn.edu>
2017-04-26 18:21:55 -0500

    Green Light |

--- softwareprocess/test/DispatchTest.py
+++ softwareprocess/test/DispatchTest.py
@@ -485,6 +485,16 @@ class DispatchTest(unittest.TestCase):
         dict['assumedLong'] = '74d35.3'
         self.assertEquals(dispatch.dispatch(dict)['error'], 'invalid assumedLat')
 
+    def test300_180ShouldGiveErrorOnNonNumericAssumedLat2(self):
+        dict = {}
+        dict['op'] = 'correct'
+        dict['lat'] = '30d30.0'
+        dict['long'] = '30d30.0'
+        dict['altitude'] = '30d30.0'
+        dict['assumedLat'] = '30dy.y'
+        dict['assumedLong'] = '74d35.3'
+        self.assertEquals(dispatch.dispatch(dict)['error'], 'invalid assumedLat')
+
 # UNIT TESTS
     def test400_010CalculateLHANominal(self):
         self.assertEquals(dispatch.calculateLocalHourAngle('95d41.6', '74d35.3'), '170d16.9')
